9aa66ab82a9820b4ff492c2085c376f475aa2f00
Nishtha Bijalwan <nbijalwa@cisco.com>
2019-04-29 23:32:27 +0530

    Use of Data Model

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by nbijalwa on 29/04/19.
+//  Copyright © 2019 nbijalwa. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
